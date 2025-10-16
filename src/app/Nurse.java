de5f89add93baf5a8a24eb76bcc986425dea6b81
Soidum_Man <bhalaninaman7@gmail.com>
2025-10-16 13:51:38 +1100

    final update

--- src/app/Nurse.java
+++ src/app/Nurse.java
@@ -4,7 +4,8 @@ public class Nurse extends Staff {
     public Nurse(String staffId, String name, Gender gender, String username, String password) {
         super(staffId, name, gender, username, password, Role.NURSE);
     }
-
+    
+    //testing
     @Override
     public void displayInfo() {
         System.out.println("Nurse: " + name + " (ID: " + staffId + ")");
